4d0e92014f5c66efae81a101fe81fedf1d5c6976
alexSh14 <alex.1486@mail.ru>
2023-07-10 23:46:59 +0700

    Добавьте файлы проекта.

---
+++ OOP Home work2 (UserAutorization)/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Home_work2__UserAutorization_;
+
+var userManager = new UserManager();
+
+// Добавляем пользователей
+userManager.AddUser("user1", "password1");
+userManager.AddUser("user2", "password2");
+userManager.AddUser("user3", "password3");
+
+// Аутентифицируем пользователей
+Console.WriteLine(userManager.Authenticate("user1", "password1")); // true
+Console.WriteLine(userManager.Authenticate("user2", "password3")); // false
+Console.WriteLine(userManager.Authenticate("user4", "password4")); // false
+
---
+++ OOP Home work2 (UserAutorization)/UserManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Home_work2__UserAutorization_
+{
+    public class UserManager
+    {
+        private List<User> users = new List<User>();
+
+        public void AddUser(string username, string password)
+        {
+            var user = new User { Username = username, Password = password };
+            users.Add(user);
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            foreach (var user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
